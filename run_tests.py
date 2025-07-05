d7dfa95764a2e0c50783e13225194a782f0479a8
Laura <laurafariacastro@outlook.com>
2025-07-05 14:49:58 -0300

    Fix paths in yml and scripts

--- run_tests.py
+++ run_tests.py
@@ -146,21 +146,22 @@ def save_stats_report(results):
     failed_tests = sum(1 for r in results if r['Status'] == 'Failed')
     skipped_tests = sum(1 for r in results if r['Status'] == 'Skipped')
     passed_percentage = (passed_tests / total_tests * 100) if total_tests > 0 else 0
+    skipped_percentage = (skipped_tests / total_tests * 100) if total_tests > 0 else 0
     failed_percentage = (failed_tests / total_tests * 100) if total_tests > 0 else 0
 
     passed_in_retry = sum(1 for r in results if r['Attempt Passed'] > 1 and r['Status'] == 'Passed')
+    passed_in_retry_percentage = (passed_in_retry / total_tests * 100) if total_tests > 0 else 0
 
 
     with open('test_report_stats.md', 'w') as stats_file:
         stats_file.write(f"# Test Report Stats\n\n")
         stats_file.write(f"Total Tests: {total_tests}\n")
-        stats_file.write(f"Passed Tests: {passed_tests}\n")
-        stats_file.write(f"Failed Tests: {failed_tests}\n")
-        stats_file.write(f"Skipped Tests: {skipped_tests}\n\n")
-        stats_file.write("## Metrics\n\n")
-        stats_file.write("Passed: " + str(passed_percentage) + "%\n")
-        stats_file.write("Passed in retry: " + str(passed_in_retry) + "%\n")
-        stats_file.write("Failed: " + str(failed_percentage) + "%\n")
+        stats_file.write(f" . | . \n")
+        stats_file.write(f"---|---\n")
+        stats_file.write(f"Passed Tests: {passed_tests} | {str(passed_percentage)}%\n")
+        stats_file.write(f"Failed Tests: {failed_tests} | {str(failed_percentage)}%\n")
+        stats_file.write(f"Skipped Tests: {skipped_tests}| {str(skipped_percentage)}%\n")
+        stats_file.write(f"Passed in retry: {passed_in_retry} |" + str(passed_in_retry_percentage) + "%\n")
 
 
     print("\nTest report stats written to test_report_stats.md", flush=True)
